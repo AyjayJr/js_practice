7dc754aecf0503783ebad9db1421dfd982e86551
AyjayJr <ayjayjr@hotmail.com>
2022-12-20 11:44:14 -0500

    added answers in count.js

--- count.js
+++ count.js
@@ -1,7 +1,7 @@
-countUnique([1,1,1,1,1,2]);            
-countUnique([1,2,3,4,4,4,7,7,12,12,13]);
-countUnique([]);
-countUnique([-2,-1,-1,0,1]);
+countUnique([1,1,1,1,1,2]);               // 2
+countUnique([1,2,3,4,4,4,7,7,12,12,13]);  // 7
+countUnique([]);                          // 0
+countUnique([-2,-1,-1,0,1]);              // 4
 
 
 // return the amound of unique values in the array
