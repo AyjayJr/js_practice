d881218f7709329fd6e4e1cb3587941971f8b61d
AyjayJr <ayjayjr@hotmail.com>
2022-12-21 12:12:18 -0500

    started implementation of minSubarrayLen which uses the sliding window pattern

---
+++ minSubarrayLen.js
@@ -0,0 +1,15 @@
+console.log(minSubarrayLen([2,3,1,2,4,3], 7));
+console.log(minSubarrayLen([2,1,6,5,4], 9));
+console.log(minSubarrayLen([3,1,7,11,2,9,8,21,62,33,19], 52));
+console.log(minSubarrayLen([1,4,16,22,5,7,8,9,10], 39));
+console.log(minSubarrayLen([1,4,16,22,5,7,8,9,10], 55));
+console.log(minSubarrayLen([1,4,16,22,5,7,8,9,10], 95));
+console.log(minSubarrayLen([4,3,3,8,1,2,3], 11));
+
+// return the min length of a subarray whose sum is greater than or equal to the sum passed to it
+function minSubarrayLen(arr, sum) {
+
+
+
+
+}
