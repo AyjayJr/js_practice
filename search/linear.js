e83d8658a980d649ba3ed33c60d0457d590fedd2
AyjayJr <ayjayjr@hotmail.com>
2022-12-26 18:30:44 -0500

    linear search implementation

---
+++ search/linear.js
@@ -0,0 +1,14 @@
+console.log(linearSearch([100], 100))                         // 0
+console.log(linearSearch([100], 200))                         // -1
+console.log(linearSearch([1,2,3,4,5], 6))                     // -1
+console.log(linearSearch([10, 15, 20, 25, 30], 15))           // 1
+console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4))  // 5
+console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10)) // -1
+
+// return the index of the key in the array if it exists, else -1 
+function linearSearch(arr, key) {
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === key) { return i; }
+   }
+   return -1;
+}
