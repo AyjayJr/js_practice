fec8c5f3432668d616e57be328ebfea9eb04644c
AyjayJr <ayjayjr@hotmail.com>
2022-12-26 16:41:49 -0500

    figured out recursive reverse using a helper function

--- recursion/reverse.js
+++ recursion/reverse.js
@@ -3,5 +3,18 @@ console.log(reverse('rithmschool')); // 'loohcsmhtir'
 
 // return a reverse of the given string recursively
 function reverse(str) {
+   ret = []; 
 
+   // helper function
+   function recurse(str) {
+      if (str === '') { return null };
+      recurse(str.slice(1));
+      ret.push(str[0]);
+      return;
+   }
+
+   recurse(str);
+   return ret.join('');
 }
+
+// feel like there is a cleaner way to do this
