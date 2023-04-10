e45a02c8dea762a03c8c72101c81d98bf7cd18ea
AyjayJr <ayjayjr@hotmail.com>
2023-04-09 23:19:55 -0400

    implemented binary search algo

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -1,25 +1,40 @@
-binarySearch([1,2,3,4,5],2) // 1
-binarySearch([1,2,3,4,5],3) // 2
-binarySearch([1,2,3,4,5],5) // 4
-binarySearch([1,2,3,4,5],6) // -1
-binarySearch([
+console.log(binarySearch([1,2,3,4,5],2)) // 1
+console.log(binarySearch([1,2,3,4,5],3)) // 2
+console.log(binarySearch([1,2,3,4,5],5)) // 4
+console.log(binarySearch([1,2,3,4,5],6)) // -1
+console.log(binarySearch([
   5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
-], 10) // 2
-binarySearch([
+], 10)); // 2
+console.log(binarySearch([
   5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
-], 95) // 16
-binarySearch([
+], 95)); // 16
+console.log(binarySearch([
   5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
-], 100) // -1
+], 100)); // -1
 
 
-const binarySearch = (arr) => {
+function binarySearch(arr, target) {
 
    let low = 0;
-   let high= arr.length - 1;
-   let mid = low + (high - low) / 2;
+   let mid = 0;
+   let high = arr.length - 1;
 
-};
+   while (low <= high) {
+      mid = Math.floor(low + ((high - low) / 2));
+      
+      if (arr[mid] == target) {
+         return mid;
+      }
+      else if (arr[mid] > target) {
+         high = mid - 1;
+      }
+      else if (arr[mid] < target) {
+         low = mid + 1;
+      }
+   }
+
+   return -1;
+}
