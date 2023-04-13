b8f36305e15a4b68e1c07b94f4f208abc39a9031
AyjayJr <ayjayjr@hotmail.com>
2023-04-12 21:37:36 -0400

    fully optimized bubble sort

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,10 +1,12 @@
-console.log(bubbleSort([5,4,3,2,1]));
+// console.log(bubbleSort([5,4,3,2,1]));
 // console.log(bubbleSort([5,2,3,1,4]));
 // console.log(bubbleSort([]));
 // console.log(bubbleSort([1,0,-1]));
 // console.log(bubbleSort([1,0]));
+console.log(bubbleSort([1,2,3,4,5]));
 
 function bubbleSort(arr) {
+   let noSwaps = true;
    for (let i = arr.length; i > 0; --i) {
       for (let j = 0; j < i - 1; ++j) {
          console.log(arr, arr[j], arr[j+1]);
@@ -12,8 +14,10 @@ function bubbleSort(arr) {
             let temp = arr[j];
             arr[j] = arr[j+1];
             arr[j+1] = temp;
+            noSwaps = false;
          }
       }
+      if (noSwaps) { break; }
    }
    return arr;
 }
